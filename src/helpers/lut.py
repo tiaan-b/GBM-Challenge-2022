967b1ab61ebbeb8ec72e3eaffdf37b5c16f9f2d2
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2022-06-06 14:58:36 -0700

    added cipher for encoding data

--- src/helpers/lut.py
+++ src/helpers/lut.py
@@ -56,3 +56,24 @@ clinical_iterables = {
     'campaign':             '#Campaign',
     'additional_id':        '#Additional ID'
 }
+
+data_cipher = {
+        'recording_locations':  {'PV': 0, 'TV': 1, 'AV': 2, 'MV': 3, 'Phc': 4},
+        'age':                  {'Neonate': 2, 'Infant': 26, 'Child': 6*52, 'Adolescent': 15*52, 'Young Adult': 20*52}, #represent each age group as the approximate number of weeks for the middle of the age group
+        'sex':                  {'Male': 0, 'Female': 1},
+        'pregnancy_status':     {'True': 1, 'False': 0},
+        'murmur':               {'Present': 1, 'Absent': 0, 'Unknown': 2},
+        'murmur_locations':     {'PV': 0, 'TV': 1, 'AV': 2, 'MV': 3, 'Phc': 4},
+        'most_audible_location':{'PV': 0, 'TV': 1, 'AV': 2, 'MV': 3, 'Phc': 4},
+        'sys_mur_timing':       {'Early-systolic': 0, 'Holosystolic': 1, 'Mid-systolic': 2, 'Late-systolic': 3},
+        'sys_mur_shape':        {'Crescendo': 0, 'Decrescendo': 1, 'Diamond': 2, 'Plateau': 3},
+        'sys_mur_pitch':        {'Low': 0, 'Medium': 1, 'High': 2},
+        'sys_mur_grading':      {'I/VI': 0, 'II/VI': 1, 'III/VI': 2},
+        'sys_mur_quality':      {'Blowing': 0, 'Harsh': 1, 'Musical': 2},
+        'dia_mur_timing':       {'Early-diastolic': 0, 'Holodiastolic': 1, 'Mid-diastolic': 2},
+        'dia_mur_shape':        {'Crescendo': 0, 'Decrescendo': 1, 'Diamond': 2, 'Plateau': 3}, #note: only decresendo and plateau are actually used, other items are included for consistency with 'systolic murmur shape'
+        'dia_mur_pitch':        {'Low': 0, 'Medium': 1, 'High': 2},
+        'dia_mur_grading':      {'I/IV': 0, 'II/IV': 1, 'III/IV': 2},
+        'dia_mur_quality':      {'Blowing': 0, 'Harsh': 1, 'Musical': 2}, #note: only blowing and harsh are actually used, other items are included for consistency with 'systolic murmur quality'
+        'outcome':              {'Abnormal': 0, 'Normal': 1}
+    }
