e5b54955c64adce0f68bd79b2655eb809bc8fe17
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2022-06-05 22:41:09 -0700

    when storing data, each recording (ie each wav file) has its own line in data

--- src/helpers/input_processor.py
+++ src/helpers/input_processor.py
@@ -10,14 +10,18 @@ import tqdm
 
 def ingest_data(data_dir):
     print("Ingesting data from ", data_dir  )
+
     # we use the deepcopy function to avoid overwriting the original clinical_iterables
     # otherwise this creates a super hard to find bug where running the code multiple times
     # will result in the output being different each time
     data = copy.deepcopy(clinical_data)
+
+    #loop through txt files in directory
     for file in os.listdir(data_dir):
         if file.endswith(".txt"):
             # open text file
             with open(data_dir + "/" + file, "r") as f:
+
                 # read first line
                 line = f.readline()
                 # split line into list
@@ -25,7 +29,8 @@ def ingest_data(data_dir):
                 data['patient_id'].append(line[0])
                 data['num_locations'].append(line[1])
                 data['sampling_frequency'].append(line[2])
-                # loop through each line to check if it maches with an iterables
+                
+                # loop through each line to check if it maches with an iterables or if it contains a wav file
                 audio_files = []
                 recording_locations = []
                 for line in f:
@@ -35,14 +40,28 @@ def ingest_data(data_dir):
                         line_split = line.split(" ")
                         audio_files.append(line_split[2]) 
                         recording_locations.append(line_split[0]) 
+                    #loop through iterables to check if line matches with any of them
                     for iterable in clinical_iterables:
                         if line.startswith(clinical_iterables[iterable] + ":"):
                             # get the value of the iterable
                             value = line.split(': ', 1)[1].strip()
                             # add the value to the data
                             data[iterable].append(value)
-                data['audio_files'].append(audio_files)
-                data['recording_locations'].append(recording_locations)
+
+                #add each audio file and corresponding recording location to data as its own line
+                #for all lines added, extend remaining columns and fill with the data collected for this file
+                num_locations = data['num_locations'][-1]
+                for name in data:
+                    if name=='audio_files':
+                        data[name].extend(audio_files)
+                    elif name=='recording_locations':
+                        data[name].extend(recording_locations)
+                    else:
+                        extend_by = num_locations - 1
+                        fill_with_value = data[name][-1]
+                        data[name].extend([fill_with_value for x in range(extend_by)])
+
+    #store data as a dataframe
     df = pl.DataFrame(data)
     return df
 
