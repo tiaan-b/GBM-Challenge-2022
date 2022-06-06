eb629ff4d80dfa6e8f8e1c7ca7a81c85c06f5916
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2022-06-06 14:06:00 -0700

    ingest_data: loads spectrograms for each wav file saves df as file, loads df from this file on successive calls to function
    files_to_spectro:
    saves each spectro as a file, return list of paths to files

--- src/helpers/input_processor.py
+++ src/helpers/input_processor.py
@@ -8,68 +8,97 @@ import copy
 import tqdm
 
 
-def ingest_data(data_dir):
-    print("Ingesting data from ", data_dir  )
+def ingest_data(data_dir, cache_folder):
 
-    # we use the deepcopy function to avoid overwriting the original clinical_iterables
-    # otherwise this creates a super hard to find bug where running the code multiple times
-    # will result in the output being different each time
-    data = copy.deepcopy(clinical_data)
+    save_file_name = 'ingested_data.json'
+    
+    #check if data has already been ingested
+    os.makedirs(cache_folder, exist_ok=True)
+    data_is_saved = False
+    #check if save file exists
+    if save_file_name in os.listdir(cache_folder):
+        saved_data = pl.read_json(cache_folder + '/' + save_file_name)
+        #check if saved data matches the desired data
+        saved_audio_files = saved_data.get_column('audio_files').to_list()
+        saved_spectros = saved_data.get_column('spectrogram').to_list()
+        desired_audio_files = [x for x in os.listdir(data_dir) if x.endswith('.wav')]
+        desired_spectros = [cache_folder + '/spectrograms/' + x.replace('.wav', '.npy') for x in desired_audio_files]
+        if set(saved_audio_files) == set(desired_audio_files) and set(saved_spectros) == set(desired_spectros):
+            data_is_saved = True
+    
+    #load df from save file if it exists, otherwise generate df from raw data
+    if data_is_saved:
+        print('loading data from save file: ', cache_folder + '/' + save_file_name)
+        df = saved_data
+    else:
+        print("Ingesting data from ", data_dir)
+        # we use the deepcopy function to avoid overwriting the original clinical_iterables
+        # otherwise this creates a super hard to find bug where running the code multiple times
+        # will result in the output being different each time
+        data = copy.deepcopy(clinical_data)
 
-    #loop through txt files in directory
-    for file in os.listdir(data_dir):
-        if file.endswith(".txt"):
-            # open text file
-            with open(data_dir + "/" + file, "r") as f:
+        #loop through txt files in directory
+        for file in os.listdir(data_dir):
+            if file.endswith(".txt"):
+                # open text file
+                with open(data_dir + "/" + file, "r") as f:
 
-                # read first line
-                line = f.readline()
-                # split line into list
-                line = line.strip().split(" ")
-                data['patient_id'].append(line[0])
-                data['num_locations'].append(line[1])
-                data['sampling_frequency'].append(line[2])
-                
-                # loop through each line to check if it maches with an iterables or if it contains a wav file
-                audio_files = []
-                recording_locations = []
-                for line in f:
-                    # check if line contains .wav
-                    if ".wav" in line:
-                        # split the line 
-                        line_split = line.split(" ")
-                        audio_files.append(line_split[2]) 
-                        recording_locations.append(line_split[0]) 
-                    #loop through iterables to check if line matches with any of them
-                    for iterable in clinical_iterables:
-                        if line.startswith(clinical_iterables[iterable] + ":"):
-                            # get the value of the iterable
-                            value = line.split(': ', 1)[1].strip()
-                            # add the value to the data
-                            data[iterable].append(value)
+                    # read first line
+                    line = f.readline()
+                    # split line into list
+                    line = line.strip().split(" ")
+                    data['patient_id'].append(int(line[0]))
+                    data['num_locations'].append(int(line[1]))
+                    data['sampling_frequency'].append(int(line[2]))
+                    
+                    # loop through each line to check if it maches with an iterables or if it contains a wav file
+                    audio_files = []
+                    recording_locations = []
+                    for line in f:
+                        # check if line contains .wav
+                        if ".wav" in line:
+                            # split the line 
+                            line_split = line.split(" ")
+                            audio_files.append(line_split[2]) 
+                            recording_locations.append(line_split[0]) 
+                        #loop through iterables to check if line matches with any of them
+                        else:
+                            for iterable in clinical_iterables:
+                                if line.startswith(clinical_iterables[iterable] + ":"):
+                                    # get the value of the iterable
+                                    value = line.split(': ', 1)[1].strip()
+                                    # add the value to the data
+                                    data[iterable].append(value)
+                                    break
 
-                #add each audio file and corresponding recording location to data as its own line
-                #for all lines added, extend remaining columns and fill with the data collected for this file
-                num_locations = int(data['num_locations'][-1])
-                for name in data:
-                    if name=='audio_files':
-                        data[name].extend(audio_files)
-                    elif name=='recording_locations':
-                        data[name].extend(recording_locations)
-                    else:
-                        extend_by = num_locations - 1
-                        fill_with_value = data[name][-1]
-                        data[name].extend([fill_with_value for x in range(extend_by)])
+                    #add each audio file and corresponding recording location to data as its own line
+                    #for all lines added, extend remaining columns and fill with the data collected for this file
+                    num_locations = int(data['num_locations'][-1])
+                    for name in data:
+                        if name=='audio_files':
+                            data[name].extend(audio_files)
+                        elif name=='recording_locations':
+                            data[name].extend(recording_locations)
+                        else:
+                            extend_by = num_locations - 1
+                            fill_with_value = data[name][-1]
+                            data[name].extend([fill_with_value for x in range(extend_by)])
 
-    #store data as a dataframe
-    df = pl.DataFrame(data)
+        #get spectrogram for each wav file
+        data['spectrogram'] = files_to_spectro(data['audio_files'], path=data_dir, output_folder=cache_folder+'/spectrograms')
+
+        #store data as a dataframe
+        df = pl.DataFrame(data)
+        df.write_json(cache_folder + '/' + save_file_name)
+        
     return df
 
-def files_to_spectro(fileArray, path="", sr=4000):
-    print("Generating spectrograms...")
+def files_to_spectro(fileArray, path="", output_folder="", sr=4000):
     length = 0
     spectros = []
     workingFileArray = []
+
+    os.makedirs(output_folder, exist_ok=True)
 
     #check whether fileArray is nested
     if any(isinstance(x, list) for x in fileArray):
@@ -89,7 +118,10 @@ def files_to_spectro(fileArray, path="", sr=4000):
         patientSpectros = []
         for file in array:
             spectro = adt.wav_to_spectro(path + "/" + file, sr=sr)
-            patientSpectros.append(spectro)
+            spectro_file = output_folder + '/' + file.replace('.wav', '.npy')
+            np.save(spectro_file, spectro)
+
+            patientSpectros.append(spectro_file)
             progress.update(1)
         spectros.append(patientSpectros)
     progress.close()
