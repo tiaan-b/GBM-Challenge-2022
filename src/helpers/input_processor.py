01d628fb77f642bee49e5093387f74c5b252a767
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2022-06-07 21:26:20 -0700

    refactored input_processor

--- src/helpers/input_processor.py
+++ src/helpers/input_processor.py
@@ -7,12 +7,7 @@ from .lut import clinical_iterables, clinical_data, data_cipher
 import copy 
 import tqdm
 
-#returns dataframe storing comprehensive data (including spectrograms) for each audio file
-#data_dir       str     path to directory storing training data (.txt and .wav files for each subject, etc)
-#cache_dir      str     path to directory storing copy of ingested data (or, if it does not yet exist, where a copy will be stored)
-#encode_data    Bool    [OPTIONAL] whether the data should be encoded
-def ingest_data(data_dir, cache_dir, encode_data=False):
-
+def loadData(data_dir, cache_dir, encode_data=False):
     #file to store ingested data, inside cache_dir directory
     cache = 'ingested_data.json'
     
@@ -23,66 +18,82 @@ def ingest_data(data_dir, cache_dir, encode_data=False):
     #load df from save file if it exists, otherwise generate df from raw data
     if data_is_saved:
         print('loading data from save file: ', cache_dir + '/' + cache)
+
         df = pl.read_json(cache_dir + '/' + cache)
     else:
-        print("Ingesting data from ", data_dir)
-        # we use the deepcopy function to avoid overwriting the original clinical_iterables
-        # otherwise this creates a super hard to find bug where running the code multiple times
-        # will result in the output being different each time
-        data = copy.deepcopy(clinical_data)
-
-        #loop through txt files in directory
-        total_txt_files = len([x for x in os.listdir(data_dir) if x.endswith('.txt')])
-        progress = tqdm.tqdm(total=total_txt_files, desc="Reading from .txt files in "+data_dir)
-        for file in os.listdir(data_dir):
-            if file.endswith(".txt"):
-                # open text file
-                with open(data_dir + "/" + file, "r") as f:
-
-                    # read first line
-                    line = f.readline()
-                    # split line into list
-                    line = line.strip().split(" ")
-                    data['patient_id'].append(int(line[0]))
-                    data['num_locations'].append(int(line[1]))
-                    data['sampling_frequency'].append(int(line[2]))
-                    
-                    # loop through each line to check if it matches with an iterables or if it contains a wav file
-                    audio_files = []
-                    recording_locations = []
-                    for line in f:
-                        # check if line contains .wav
-                        if ".wav" in line:
-                            # split the line 
-                            line_split = line.split(" ")
-                            audio_files.append(line_split[2]) 
-                            recording_locations.append(line_split[0]) 
-                        #loop through iterables to check if line matches with any of them
-                        else:
-                            for iterable in clinical_iterables:
-                                if line.startswith(clinical_iterables[iterable] + ":"):
-                                    # get the value of the iterable
-                                    value = line.split(': ', 1)[1].strip()
-                                    # add the value to the data
-                                    data[iterable].append(value)
-                                    break
-                                
-                    data['audio_files'].append(audio_files)
-                    data['recording_locations'].append(recording_locations)
-                    progress.update(1)
-        progress.close()
-
+        print("loading raw data from ", data_dir)
+        
+        df = ingest_data(data_dir)
         #get spectrogram for each wav file
-        data['spectrogram'] = files_to_spectro(data['audio_files'], path=data_dir, output_folder=cache_dir+'/spectrograms')
+        length = df.height
+        progress = tqdm.tqdm(total=length, desc="Generating spectrograms")
+        df = df.with_column(
+            pl.col('audio_files')
+            .apply(lambda x: __function_with_logUpdater(progress, files_to_spectro(x, path=data_dir, output_folder=cache_dir+'/spectrograms')))
+            .alias('spectrogram')
+        )
+        progress.close()
         #explode data so that each audio file and its corresponding recording location, spectrogram is on its own line in data
-        data = reshapeData(data)
-        #store data as a dataframe
-        df = pl.DataFrame(data)
+        nested_data = ['audio_files', 'recording_locations', 'spectrogram']
+        df = df.explode(nested_data)
+        #save df to file. Future calls to loadData will load the df from this file
         df.write_json(cache_dir + '/' + cache)
 
     if encode_data:
-        encoded_data = encodeData(df.to_dict())
-        df = pl.DataFrame(encoded_data)
+        df = encodeData(df)
+
+    return df
+
+
+def ingest_data(data_dir):
+
+    # we use the deepcopy function to avoid overwriting the original clinical_iterables
+    # otherwise this creates a super hard to find bug where running the code multiple times
+    # will result in the output being different each time
+    data = copy.deepcopy(clinical_data)
+
+    #loop through txt files in directory
+    total_txt_files = len([x for x in os.listdir(data_dir) if x.endswith('.txt')])
+    progress = tqdm.tqdm(total=total_txt_files, desc="Reading from .txt files in "+data_dir)
+    for file in os.listdir(data_dir):
+        if file.endswith(".txt"):
+            # open text file
+            with open(data_dir + "/" + file, "r") as f:
+
+                # read first line
+                line = f.readline()
+                # split line into list
+                line = line.strip().split(" ")
+                data['patient_id'].append(int(line[0]))
+                data['num_locations'].append(int(line[1]))
+                data['sampling_frequency'].append(int(line[2]))
+                
+                # loop through each line to check if it matches with an iterables or if it contains a wav file
+                audio_files = []
+                recording_locations = []
+                for line in f:
+                    # check if line contains .wav
+                    if ".wav" in line:
+                        # split the line 
+                        line_split = line.split(" ")
+                        audio_files.append(line_split[2]) 
+                        recording_locations.append(line_split[0]) 
+                    #loop through iterables to check if line matches with any of them
+                    else:
+                        for iterable in clinical_iterables:
+                            if line.startswith(clinical_iterables[iterable] + ":"):
+                                # get the value of the iterable
+                                value = line.split(': ', 1)[1].strip()
+                                # add the value to the data
+                                data[iterable].append(value)
+                                break
+
+                data['audio_files'].append(audio_files)
+                data['recording_locations'].append(recording_locations)
+                progress.update(1)
+    progress.close()
+
+    df = pl.DataFrame(data)
 
     return df
 
@@ -92,7 +103,7 @@ def reshapeData(data):
     if isinstance(data, dict):
         data_type = 'dict'
         input_data = data.copy()
-    elif isinstance(data, polars.internals.frame.DataFrame):
+    elif isinstance(data, pl.internals.frame.DataFrame):
         data_type = 'DataFrame'
         input_data = data.to_dict()
     else:
@@ -123,6 +134,12 @@ def reshapeData(data):
     return out
 
 
+def __function_with_logUpdater(progress, func):
+    progress.update(1)
+    out = func
+    return out
+
+
 def files_to_spectro(fileArray, path="", output_folder="", sr=4000):
     length = 0
     spectros = []
@@ -143,7 +160,7 @@ def files_to_spectro(fileArray, path="", output_folder="", sr=4000):
     for array in workingFileArray:
         length += len(array)
 
-    progress = tqdm.tqdm(total=length, desc="Generating spectrograms")
+    #progress = tqdm.tqdm(total=length, desc="Generating spectrograms")
     for array in workingFileArray:
         patientSpectros = []
         for file in array:
@@ -152,9 +169,9 @@ def files_to_spectro(fileArray, path="", output_folder="", sr=4000):
             np.save(spectro_file, spectro)
 
             patientSpectros.append(spectro_file)
-            progress.update(1)
+            #progress.update(1)
         spectros.append(patientSpectros)
-    progress.close()
+    #progress.close()
 
     #return an un-nested list if the array passed as fileArray was also un-nested
     if not isNested:
@@ -179,20 +196,36 @@ def __checkCache(data_dir, cache_dir, cache):
 
 
 def encodeData(data):
-    working_data = data.copy()
+    #check if data is a polars dataframe
+    if not isinstance(data, pl.internals.frame.DataFrame):
+        raise Exception('data is of unsupported type "{}". Supported types include polars.internals.frame.DataFrame'.format(data.type()))
+
     cipher = data_cipher.copy()
+    data_columns = data.columns
+    numeric_data = [x for x in data_columns if x in ['patient_id', 'num_locations', 'sampling_frequency', 'height', 'weight', 'additional_id']]
+    unencoded_data = [x for x in data_columns if x in ['audio_files', 'spectrogram']]
+    cipher_friendly_data = [x for x in data_columns if x in cipher.keys() and x!='murmur_locations']
 
-    #cast numeric data to float type
-    for column in ['patient_id', 'num_locations', 'sampling_frequency', 'height', 'weight', 'additional_id']:
-        working_data[column] = [float(x) for x in working_data[column]]
+    out = data.select([
+        #include unencoded data
+        pl.col(unencoded_data),
 
-    #for each entry in data['murmur_locations']: split into list, then encode each element of list using cipher
-    mod_entry = lambda entry: [cipher['murmur_locations'][y] for y in entry.strip().split('+')]
-    working_data['murmur_locations'] = [mod_entry(x) for x in working_data['murmur_locations']]
+        #cast numeric data to float type
+        pl.col(numeric_data)
+        .cast(pl.datatypes.Float64),
 
-    #encode remaining data
-    for column in cipher:
-        if not column=='murmur_locations':
-            working_data[column] = [cipher[column][x] for x in working_data[column]]
+        #for each element in murmur_locations: split into list, then encode each element of list using cipher
+        pl.col('murmur_locations').str.split(by='+')
+        .arr.eval(pl.element().apply(lambda x: cipher['murmur_locations'][x])),
 
-    return working_data
+        #encode remaining data using cipher
+        pl.col(cipher_friendly_data)
+        .map(lambda x: __applyCipher(x, cipher))
+    ])
+
+    return out
+
+def __applyCipher(col, cipher):
+    col_name = col.name
+    out = col.apply(lambda x: cipher[col_name][x])
+    return out
